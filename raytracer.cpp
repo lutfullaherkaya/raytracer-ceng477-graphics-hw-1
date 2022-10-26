c47287092e69c88d2890c5fa6a9d5427cfbd2217
lütfullah <lerkaya0@gmail.com>
2022-10-26 14:39:29 +0300

    raytracer temeli atıldı

--- raytracer.cpp
+++ raytracer.cpp
@@ -4,7 +4,42 @@
 
 typedef unsigned char RGB[3];
 
-int main(int argc, char* argv[])
+class RayTracer
+{
+public:
+    RayTracer(parser::Scene &scene) {
+        this->scene = scene;
+    }
+    void rayTrace()
+    {
+        for (auto camera : scene.cameras)
+        {
+            unsigned char *image = new unsigned char[camera.image_width * camera.image_height * 3];
+            for (int i = 0; i < camera.image_height; i++)
+            {
+                for (int j = 0; j < camera.image_width; j++)
+                {
+                    RGB raytracedColor = {0, 0, 0};
+
+                    // do raytrace
+
+                    image[i++] = raytracedColor[0];
+                    image[i++] = raytracedColor[1];
+                    image[i++] = raytracedColor[2];
+                }
+            }
+            write_ppm(camera.image_name.c_str(), image, camera.image_width , camera.image_height);
+            std::cout << camera.image_name;
+        }
+        
+    }
+
+private:
+    parser::Scene scene;
+
+};
+
+int main(int argc, char *argv[])
 {
     // Sample usage for reading an XML scene file
     parser::Scene scene;
@@ -18,35 +53,6 @@ int main(int argc, char* argv[])
     // Normally, you would be running your ray tracing
     // code here to produce the desired image.
 
-    const RGB BAR_COLOR[8] =
-    {
-        { 255, 255, 255 },  // 100% White
-        { 255, 255,   0 },  // Yellow
-        {   0, 255, 255 },  // Cyan
-        {   0, 255,   0 },  // Green
-        { 255,   0, 255 },  // Magenta
-        { 255,   0,   0 },  // Red
-        {   0,   0, 255 },  // Blue
-        {   0,   0,   0 },  // Black
-    };
-
-    int width = 640, height = 480;
-    int columnWidth = width / 8;
-
-    unsigned char* image = new unsigned char [width * height * 3];
-
-    int i = 0;
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            int colIdx = x / columnWidth;
-            image[i++] = BAR_COLOR[colIdx][0];
-            image[i++] = BAR_COLOR[colIdx][1];
-            image[i++] = BAR_COLOR[colIdx][2];
-        }
-    }
-
-    write_ppm("test.ppm", image, width, height);
-
+    RayTracer raytracer(scene);
+    raytracer.rayTrace();
 }
